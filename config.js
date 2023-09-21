9f1b6c3099bd7d83021176a946325712d21d07fa
Hidrocarburo <56695126+Hidrocarburo@users.noreply.github.com>
2023-09-20 22:09:24 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
     PORT: 8080,
-    MONGODB_CONNECTION_STR: 'mongodb+srv://Hidrocarburo:1234qwer@cluster0.z6qan3p.mongodb.net/ecommerce?retryWrites=true&w=majority',
+    MONGODB_CONNECTION_STR: '',
     MONGODB_TIMEOUT: 5000,
     PRODUCT_IMAGES_FOLDER: './public/products', // Debe estar dentro de public.
     URL_PRODUCT_API: '/api/products',
